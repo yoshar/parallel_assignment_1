acb0258f3e3b2a84467f954cf6bc5fa384987e21
yoshariley <yoshar@knights.ucf.edu>
2023-01-27 22:15:15 -0500

    readme done and outputs to txt

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Parallel Computing Assignment 1
+This is a multithreaded application which sums, counts and lists the maximum 10 primes below a limit, in the scope of the assignment this is 10<sup>8</sup>. This was written in Spring 2023 for the Parallel Computing class at UCF.
+
+This is a fairly fast iterative implementation, however if you are looking at this in a future class I would direct you to implementing a sieve.
+
+## To Compile and Run
+cargo run
+
+Test cases, set at values for 10<sup>8</sup>
+cargo test
+
+## Reasoning and Design
+I use a mpsc pattern to assign each thread a starting number and the limit, those threads then check the primality of each number within that range and according to their offset. The threads then return a vector the found primes to the receiver. The receiver preforms all counting and adding operations needed. 
+
+The two important parts are the offset and the primality check I used. For the offset each thread is given its starting number then it will increment by 1 + n_threads + thread_id (ex. thread 1 will start on 1 and then move to 11), this ensures that all threads will now be on an odd number and offset from each other. For the rest of the iteration each thread will increment by 16 (for 8 threads) to skip over what each thread is doing or will do and to skip the next odd number. This effectively reduces n by half as even numbers, excluding 2, cannot be prime. Doing the indices this way ensures that each thread is doing close to the same amount of work, without micro-managing them which would slow them down.
+
+The primality test I used is a deterministic version the miller-rabin test. I talk a bit about it in the comments above the method. To use it on any number n it would need to be randomized, however mine is deterministic up to 2147483647, which is more than 10<sup>8</sup>. Within the scope of the project the implementation of the miller-rabin test that I used is deterministic.
+
+## Experimentation Evaluation
+There are two separate scalers to the time taken of this algorithm, the multithreading implementation, and the primality test.
+
+I started with a naive approach that takes every prime in a range and checks it. Then each thread will get a range and an offset to use to check them. Without excluding odd numbers 4 threads will always be checking even numbers which are trivial. This led me to trying a thread pool to have the master thread give each number, but this ended up being slow as there is too much overhead where the threads are passing information. I went back to the mpsc and made my indices smarter, this improved run time significantly as I was now only running n/2 instead of n.
+
+After this improvement I went from ~80s to ~21s
+
+Next I used the primal library and saw a huge performance increase using their primality test over mine. I wasn't sure if I was allowed to use that library and it seemed like it was within the scope of the project to code my own so that is what I did.
+
+Primal performance boost ~21s to ~5s
+
+I read in the primal documentation that they used a deterministic version of the miller-rabin test and researched that further and found some info on it and redid a C implementation into rust. This ended up being faster than the one in the primal library.
+
+With all of this I was down from ~80s to ~2.5s
+
+## Current Runtime
+System
+i7-9700k @ stock (3.6gHz - 4.9gHz boost) 8c/8t
+2.60s
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::{thread, mem};
 use std::sync::{mpsc};
 use std::time::{Instant};
-// use primal::is_prime;
-// use rand::Rng;
+use std::fs;
 
 fn main() {
     let start = Instant::now();
@@ -16,6 +15,10 @@ fn main() {
     println!("Number of Primes: {:?}", n_primes);
     println!("Top 10 Primes: {:?}", list_primes);
     println!("Time elapsed: {:?}", duration);
+
+    let s = format!("{:?} {:?} {:?}\n{:?}", duration, n_primes, sum, list_primes);
+    
+    fs::write("primes.txt", s).expect("Unable to write/create file");
 }
 
 fn list_primes(n: i32, nthreads: i32) -> (i64, i32, [i32; 10]) {
@@ -23,11 +26,12 @@ fn list_primes(n: i32, nthreads: i32) -> (i64, i32, [i32; 10]) {
     let mut n_primes: i32 = 0;
     
     let mut max_list = [0; 10];
-    let mut count: usize = 0;
     let mut min: i32 = 0;
 
+    // initialize mpsc
     let (sender, reciever) = mpsc::channel();
 
+    // initialize threads
     for i in 0..nthreads {
         let sender_n = sender.clone();
         thread::spawn(move || {
@@ -36,17 +40,13 @@ fn list_primes(n: i32, nthreads: i32) -> (i64, i32, [i32; 10]) {
         });
     }
 
+    // drop all threads otherwise reciever will infinitely wait
     mem::drop(sender);
 
     for recieved in reciever {
         for i in recieved {
             n_primes += 1;
             sum += i as i64;
-
-            if count < 9 {
-                max_list[count] = i;
-                count += 1;
-            }
 
             if i > min {
                 max_list.sort();
@@ -73,7 +73,7 @@ fn prime_calc(nthreads: i32, thread_number: i32, n: i32, sender: mpsc::Sender<Ve
             send.push(i);
         }
 
-        // sets offset based on thread number
+        // sets offset based on thread number and sets the next iteration on odd numbers, even numbers excl 2 cant be prime
         if i == thread_number {
             i = i + 1 + nthreads + thread_number;
         }
@@ -92,7 +92,6 @@ fn minimum(a: i32, b: i32) -> i32 {
     if a < b {
         return a;
     }
-
     b
 }
 
@@ -115,7 +114,7 @@ fn minimum(a: i32, b: i32) -> i32 {
 //     true
 // }
 
-// power function so that the 
+// power function so that the operation x^y % p doesnt overflow
 fn power (x_arg: i32, y_arg: i32, p_arg: i32) -> u64 {
     let mut res: u64 = 1;
     let mut x = x_arg as u64;
@@ -135,8 +134,10 @@ fn power (x_arg: i32, y_arg: i32, p_arg: i32) -> u64 {
     res
 }
 
+// computes a^d == 1 % n OR a^(d2^r) == -1 % n 
+// returns the boolean value of them
 fn witness(n: i32, arg_s: i32, d: i32, a: i32) -> bool {
-    let mut x = power(a, d, n);
+    let mut x = power(a, d, n); // result of a^d == 1 % n
     let mut s = arg_s as u64;
     let mut y: u64 = 0;
 
@@ -158,23 +159,40 @@ fn witness(n: i32, arg_s: i32, d: i32, a: i32) -> bool {
     true
 }
 
+// C implementation this is based on
+// https://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Deterministic_up_to_341,550,071,728,321
+// Math discussion this is based on
+// https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Mathematical_concepts
+// deterministic version of the miller - rabin test
+// if you can prove a^d == 1 mod n OR a^(d2^r) == -1 mod n (where 0 <= r < s)
+// then the n is probably a prime 
+// therefore if n is not a prime base to a, then n is definitely composite
+// normally in this algorithm, the witness, a is chosen at random
+// here a is strategiaclly chosen based on the range the number is in 
+// since the base is not randomly chosen, the output it is deterministic
 fn determ_miller_test(n: i32) -> bool {
+    // check for easy primes , % 2 and % 3
     if (n % 2 == 0 && n != 2) || n < 2 || (n % 3 == 0 && n != 3) {
         return false;
     }
 
-    if n <= 3 {
+    //edge cases
+    if n == 2  || n == 3 {
         return true;
     }
 
+    
     let mut d = n / 2;
     let mut s = 1;
 
+    // determine s where n - 1 = d * 2^s
+    // rewrite the number as d times a power of 2 where s is that power
     while d % 2 == 0 {
         d = d / 2;
         s += 1;
     }
 
+    //chose a based on the range of numbers
     if n < 1373653 {
         return witness(n, s, d, 2) && witness(n, s, d, 3);
     }
@@ -187,6 +205,8 @@ fn determ_miller_test(n: i32) -> bool {
         return witness(n, s, d, 2) && witness(n, s, d, 7) && witness(n, s, d, 61);
     }
 
+    // if exceeding the range test the first 7 primes, this will not give 100% accuracy but will still be fairly accurate
+    // this case is never used in the range of 0..10e8
     witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11) && witness(n, s, d, 13) && witness(n, s, d, 17)
 }
 
@@ -215,12 +235,6 @@ mod tests {
             assert!(list[i] == test_list[i], "Incorrect list of max 10 primes");
         }
     }
-
-    // #[test]
-    // fn prime_check() {
-    //     assert!(custom_is_prime(13), "is_prime rejected a prime");
-    //     assert!(!custom_is_prime(10), "is_prime accepted a non-prime");
-    // }
 
     #[test]
     fn miller_test() {
